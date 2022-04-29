aaf465d0d0a5088b552353e18b033486f8bb5abe
benjamincloquet <benjamincloquet@users.noreply.github.com>
2022-04-29 13:48:08 +0000

    deploy: 0dcff992b3a9a43dd60bb30c09befd2617f4b18a

--- _nuxt/static/1651239849/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1651239849",layout:"default",error:a,state:{ui:{visibleSectionTitle:""},i18n:{routeParams:{}}},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"bf883b2f"}}}}(null,"\u002F"));
