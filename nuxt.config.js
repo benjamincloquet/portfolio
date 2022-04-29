c3ba4424431fdf27f4129257eac64650cb75dd44
benjamincloquet <benjamin.cloquet@gmail.com>
2022-04-29 15:55:39 +0200

    Add meta description

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,11 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Benjamin Cloquet, Software Engineer',
+      },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [
