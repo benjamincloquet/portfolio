6a61e5cd6017f8641ecc140f0c2a6614a9af1c02
benjamincloquet <benjamincloquet@users.noreply.github.com>
2022-04-29 13:44:10 +0000

    deploy: ecb5810e53d045f609ea15e0e6c701ddb80e6246

--- _nuxt/static/1651238798/fr/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1651238798",layout:"default",error:a,state:{ui:{visibleSectionTitle:""},i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Ffr",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"815e4a9e"}}}}(null));
--- _nuxt/static/1651238798/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1651238798",layout:"default",error:a,state:{ui:{visibleSectionTitle:""},i18n:{routeParams:{}}},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"815e4a9e"}}}}(null,"\u002F"));
